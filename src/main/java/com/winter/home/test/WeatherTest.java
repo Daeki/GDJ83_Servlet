1545d486209f14d98dbdb9fe01fd8a37fa6df22c
Daeki <kimdaeki@gmail.com>
2024-06-12 17:17:11 +0900

    2

---
+++ src/main/java/com/winter/home/test/WeatherTest.java
@@ -0,0 +1,13 @@
+package com.winter.home.test;
+
+import com.winter.home.weather.WeatherDAO;
+
+public class WeatherTest {
+
+	public static void main(String[] args) {
+		WeatherDAO weatherDAO = new WeatherDAO();
+		// weatherDAO.getWeather();
+
+	}
+
+}
